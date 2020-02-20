70332786b3e0a06df55cc37d815773928a93e180
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-20 02:42:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
+ # put argument(s) here
   # code here
+collection = [1, 2, 3, 4]
+
+def my_each(collection)
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+    collection
+   else
+
+     puts i
+ end
+
+my_each(collection) do |i|
+  puts i
 end
